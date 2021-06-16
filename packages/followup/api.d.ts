504035e4a281fda95a3490e61d11fc977fa49223
Irakli Gozalishvili <contact@gozala.io>
2021-06-15 23:09:13 -0700

    chore: restructure repo

--- packages/followup/api.d.ts
+++
@@ -1,7 +0,0 @@
-export type PendingStatus = import('./Followup').PendingStatus
-export type PendingPin = import('./Followup').PendingPin
-
-export class FollowupAPI {
-  constructor (config: { endpoint: URL, username: string, password: string })
-  register (pins: Iterable<PendingPin>): Promise<void>
-}
